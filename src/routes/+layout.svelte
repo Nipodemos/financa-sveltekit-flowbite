f8807b19f59bdb5224d0c6a0b8b3dc505d98440a
Nipodemos <nipodemos13@gmail.com>
2024-06-26 21:30:48 -0300

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,60 @@
+<script>
+	import { page } from '$app/stores';
+	import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
+	import {
+		ChartPieSolid,
+		GridSolid,
+		MailBoxSolid,
+		UserSolid,
+		ArrowRightToBracketOutline,
+		EditOutline
+	} from 'flowbite-svelte-icons';
+	$: activeUrl = $page.url.pathname;
+</script>
+
+<div class="flex">
+	<Sidebar {activeUrl}>
+		<SidebarWrapper>
+			<SidebarGroup>
+				<SidebarItem label="Dashboard" href="/">
+					<svelte:fragment slot="icon">
+						<ChartPieSolid
+							class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					</svelte:fragment>
+				</SidebarItem>
+				<SidebarItem label="Accordion" href="/docs/components/accordion">
+					<svelte:fragment slot="icon">
+						<GridSolid
+							class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					</svelte:fragment>
+				</SidebarItem>
+				<SidebarItem label="Alert" href="/docs/components/alert">
+					<svelte:fragment slot="icon">
+						<MailBoxSolid
+							class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					</svelte:fragment>
+				</SidebarItem>
+				<SidebarItem label="Sidebar" href="/docs/components/sidebar">
+					<svelte:fragment slot="icon">
+						<UserSolid
+							class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					</svelte:fragment>
+				</SidebarItem>
+				<SidebarItem label="Badge" href="/docs/components/badge">
+					<svelte:fragment slot="icon">
+						<ArrowRightToBracketOutline
+							class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					</svelte:fragment>
+				</SidebarItem>
+			</SidebarGroup>
+		</SidebarWrapper>
+	</Sidebar>
+	<div class="flex-1">
+		<slot></slot>
+	</div>
+</div>
